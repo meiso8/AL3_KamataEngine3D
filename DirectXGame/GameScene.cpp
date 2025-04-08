a766547682ddf022990b3028d569ae6dae1554bb
あ <k024g0101@g.neec.ac.jp>
2025-04-08 17:51:56 +0900

    KamataEngineを入れた

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,22 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+
+void GameScene::Initialize() {
+    //メンバ変数への代入処理
+
+    //ここにインゲームの初期化処理を書く
+};
+
+void GameScene::Update() {
+
+    //ここにインゲームの更新処理を書く
+
+};
+
+
+void GameScene::Draw() {
+
+
+};
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,57 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+
+// KamataEngineを構成する各クラスは基本的に KamataEngine namespaceに所属している
+// 　これを宣言することで毎度 KamataEngine:: と入力する必要がなくなる
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE2A_18_ヨシダ_トモカ_AL3");
+
+	// DirectXCommonのインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンスの取得
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始　描画ターゲットなどの設定
+		dxCommon->PreDraw();
+
+		//
+		// ここに描画処理を記述する
+		//
+
+		// ゲームシーンの描画
+		gameScene->Update();
+
+		// 描画終了　描画コマンドの実行
+		dxCommon->PostDraw();
+	}
+
+	//ゲームシーンの解放
+	delete gameScene;
+	//nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了処理
+	KamataEngine::Finalize();
+
+	return 0;
+}
